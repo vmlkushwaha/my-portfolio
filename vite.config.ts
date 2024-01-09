9fa0f77cb494bd881ddf78ddb0f38e22ccbbde04
Vimal Kushwaha <mackbook@MackBooks-MacBook-Air.local>
2024-01-09 20:51:43 +0530

    title changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import react from "@vitejs/plugin-react";
+// import ReactRefresh from "@vitejs/plugin-react-refresh";
+import { defineConfig } from "vite";
+// import legacy from "vite-plugin-legacy";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    // ReactRefresh(),
+    // legacy({ targets: ["defaults", "not IE 11"] }),
+  ],
+  base: "vmlkushwaha.github.io",
+});
